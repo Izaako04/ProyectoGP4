4dbf954ca9beb2190f7372a81298d378ca4dc992
Izaako04 <isaac.criollo04@gmail.com>
2023-07-16 18:09:52 -0500

    Util - Metodo enviarcorreo

--- ProyectoG4/src/main/java/ec/edu/espol/util/Util.java
+++ ProyectoG4/src/main/java/ec/edu/espol/util/Util.java
@@ -12,7 +12,14 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
 import java.util.Scanner;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 public class Util {
@@ -68,4 +75,30 @@ public class Util {
             System.out.println(e.getMessage());
         }
     }
+    public void enviarCorreo(String destinatario, String asunto, String cuerpo,String correo, String clave) {
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.user", correo);
+        props.put("mail.smtp.clave", correo);    //La clave de la cuenta
+        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+        
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+        
+        try {
+        message.setFrom(new InternetAddress(correo));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
+        message.setSubject(asunto);
+        message.setText(cuerpo);
+        Transport transport = session.getTransport("smtp");
+        transport.connect("smtp.gmail.com", correo, clave);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }        
 }
